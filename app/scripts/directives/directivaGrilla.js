ade1225219911dd1dc62f43911f61a97206f24ea
danag <le_bonanni@yahoo.com.ar>
2016-11-15 09:20:02 -0300

    add boostrap grid directive

---
+++ app/scripts/directives/directivaGrilla.js
@@ -0,0 +1,35 @@
+
+angular
+.module('tplaboratorioIv2016App')
+.directive('directivaCabecera',function($compile){
+	
+	return {/* 
+			scope:{
+				cabecera: '@cabecera'
+			},*/
+			template:'<th>{{columna}}</th>'
+			};
+});
+
+angular
+.module('tplaboratorioIv2016App')
+.directive('directivaFila',function(){
+	
+	return {/*
+			scope:{
+					elemento: '=elementofila'
+			},*/
+			/*replace:true, 
+			restrict:"EA", 
+			templateUrl:'templates/templateRow.html'*/
+			template: '<td ng-bind="row.nombre"></td><td ng-bind="row.apellido"></td><td ng-bind="row.mail"></td><td><button class="btn btn-default" type="submit">Borrar</button></td>'
+
+	};
+});
+
+angular
+.module('tplaboratorioIv2016App')
+.directive('directivaGrilla',function(){
+	
+	return { replace:true, restrict:"E" , templateUrl:'templates/templateGrilla.html'};
+});
