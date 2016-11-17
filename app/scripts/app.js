14dfd0b92cc2e2af2afc97b858734307be9ac3a7
danag <le_bonanni@yahoo.com.ar>
2016-11-17 01:21:52 -0300

    Se agregan rutas en el app y en el index, se agrega grilla de productos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,14 +15,40 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'satellizer'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider,$authProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
+      })
+      .when('/productos', {
+        templateUrl: 'views/productos.html',
+        controller: 'ProductosCtrl',
+        controllerAs: 'productos'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .when('/usuarios', {
+        templateUrl: 'views/usuarios.html',
+        controller: 'UsuariosCtrl',
+        controllerAs: 'usuarios'
+      })
+      .when('/altaProducto', {
+        templateUrl: 'views/altaproducto.html',
+        controller: 'AltaproductoCtrl',
+        controllerAs: 'altaProducto'
+      })
+      .when('/altaUsuario', {
+        templateUrl: 'views/altausuario.html',
+        controller: 'AltausuarioCtrl',
+        controllerAs: 'altaUsuario'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,45 @@
  * Controller of the tplaboratorioIv2016App
  */
 angular.module('tplaboratorioIv2016App')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope,productos,$cookies) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    
+    $cookies.put('unvalor','valor');
+
+    console.info("valor de la cookie -",$cookies.get('unvalor'));
+
+    $scope.grilla = {};
+
+	$scope.grilla.columnas = ['nombre','apellido','mail'];
+
+	productos.TraerTodosLosProductos().then(function(rta){
+		console.info(rta.data);
+		$scope.grilla.datos = rta.data;
+	},function(error){
+		console.info("error",error);
+	});
+
+
+	/*$scope.grilla.datos = [
+		{
+			'nombre': 'lucas',
+			'apellido': 'bonanni',
+			'mail': 'asdf@bonanni.com'
+		},
+		{
+			'nombre': 'pedro',
+			'apellido': 'asdflasdf',
+			'mail': 'asdf@bonanni.com'
+		},
+		{
+			'nombre': 'manuel',
+			'apellido': 'suarez',
+			'mail': 'asdf@bonanni.com'
+		}
+	];*/
+	
   });
