08f125430aff315e61dff1f7f82887ea3249a76c
lucasbonanni <lucas_eb2004@yahoo.com.ar>
2016-12-08 18:06:31 -0300

    add main carousel directive

---
+++ preApp/scripts/directives/carouselDirective.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tplaboratorioIv2016App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tplaboratorioIv2016App
+ */
+ angular.module('tplaboratorioIv2016App')
+   .directive('carouselPrincipal', function (){
+
+     return{
+       restrict: 'A',
+       scope:{
+         imagenes:'='
+       },
+       replace: true,
+       templateUrl:'templates/Carousel.html',
+       link: function( scope, elements, attrs, ctrl){
+
+       }
+     }
+
+   });
