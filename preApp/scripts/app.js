a562fd75a3955383c3305724bd8f35dd6d2f4cd0
lucasbonanni <lucas_eb2004@yahoo.com.ar>
2016-12-12 15:03:04 -0300

    configure ui route to solve the link to section in the same html

--- preApp/scripts/app.js
+++ preApp/scripts/app.js
@@ -19,7 +19,7 @@ angular
     'satellizer',
     'ui.router'
   ])
-.config(function ($authProvider,$stateProvider,$urlRouterProvider) {
+  .config(function($authProvider, $stateProvider, $urlRouterProvider) {
     $authProvider.baseUrl = 'http://localhost'
     $authProvider.loginUrl = '/personaSlim/ws1/login';
     $authProvider.tokenName = 'MiTokenGeneradoEnPHP';
@@ -29,68 +29,72 @@ angular
     $authProvider.withCredentials = false;
 
     $stateProvider
-    .state('main', {
-      url:'/',
-      templateUrl: 'views/shop/home.html',
-      controller: 'HomeCtrl'
-    })
-    .state('product', {
-      url:'/product_detail?id',
-      templateUrl: 'views/shop/product.html',
-      controller: 'ProductCtrl'
-    })
-    .state('productSummary', {
-      url:'/product_summary',
-      templateUrl: 'views/shop/product_summary.html',
-      controller: 'ProductSummaryCtrl'
-    })
-    .state('login', {
-        url:'/login',
+      .state('main', {
+        url: '/',
+        templateUrl: 'views/shop/home.html',
+        controller: 'HomeCtrl'
+      })
+      .state('product', {
+        url: '/product_detail?id',
+        templateUrl: 'views/shop/product.html',
+        controller: 'ProductCtrl'
+      })
+      .state('productSummary', {
+        url: '/product_summary',
+        templateUrl: 'views/shop/product_summary.html',
+        controller: 'ProductSummaryCtrl'
+      })
+      .state('login', {
+        url: '/login',
         templateUrl: 'views/shop/login.html',
         controller: 'LoginCtrl'
-    })
-    .state('register',{
-        url:'/register',
+      })
+      .state('register', {
+        url: '/register',
         templateUrl: 'views/shop/register.html',
         controller: 'RegisterCtrl'
-    })
-    .state('contact',{
-        url:'/contact',
+      })
+      .state('contact', {
+        url: '/contact',
         templateUrl: 'views/shop/contact.html',
         controller: 'ContactCtrl'
-    })
-    .state('usuarios',{
-        url:'/usuarios',
+      })
+      .state('usuarios', {
+        url: '/usuarios',
         templateUrl: 'views/usuarios.html',
         controller: 'UsuariosCtrl'
-    })
-    .state('altaProducto', {
-        url:'/altaProducto?id',
+      })
+      .state('altaProducto', {
+        url: '/altaProducto?id',
         templateUrl: 'views/altaproducto.html',
         controller: 'AltaproductoCtrl'
       })
-    .state('altaUsuario', {
-        url:'/altaUsuario?id',
+      .state('altaUsuario', {
+        url: '/altaUsuario?id',
         templateUrl: 'views/altausuario.html',
         controller: 'AltausuarioCtrl'
-    })
+      })
     $urlRouterProvider.otherwise('/');
   })
-  .run(function ($rootScope, $location/*, servicioLogin,permisosFactory*/,$auth) {
-        $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams, options) {
-            /*permisosFactory.getPayload();
-            $rootScope.isAdministrator = permisosFactory.isAdministrator();
-            $rootScope.isUser = permisosFactory.isUser();
-            console.info("state change isAdmin",permisosFactory.isAdministrator());
-            console.info("state change isAuthenticated",$auth.isAuthenticated());
+  .run(function($rootScope, $location /*, servicioLogin,permisosFactory*/ , $auth) {
+    $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams, options) {
+      var hash = $location.hash();
+      if (hash) {
+        toParams['#'] = hash;
+      }
+      /*permisosFactory.getPayload();
+      $rootScope.isAdministrator = permisosFactory.isAdministrator();
+      $rootScope.isUser = permisosFactory.isUser();
+      console.info("state change isAdmin",permisosFactory.isAdministrator());
+      console.info("state change isAuthenticated",$auth.isAuthenticated());
 
-            if(servicioLogin.isAuthenticated()){
+      if(servicioLogin.isAuthenticated()){
 
-              //get payload of authFactory
-              //if is authenticated do nothing
-            } else
-            {
-              //if not autenticated redirecte to home
-            }*/
-        });
+        //get payload of authFactory
+        //if is authenticated do nothing
+      } else
+      {
+        //if not autenticated redirecte to home
+      }*/
     });
+  });
