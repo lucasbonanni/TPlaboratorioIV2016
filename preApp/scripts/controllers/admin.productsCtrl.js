4ac9803f44e5789a1c716656c59eded86652c2a7
lucasbonanni <lucas_eb2004@yahoo.com.ar>
2017-06-20 19:56:21 -0300

    add image slide into admin product view

--- preApp/scripts/controllers/admin.productsCtrl.js
+++ preApp/scripts/controllers/admin.productsCtrl.js
@@ -9,97 +9,119 @@
  */
 
 angular.module('tplaboratorioIv2016App')
-  .controller('productsCtrl', function ($scope, FileUploader) {
-    $scope.myData = [{
-      "nombre": "Cox",
-      "descripcion": "Carney",
-      "precio": "$10",
-      "tipo": "Enormo",
-      "descuento": true
-    }, {
-      "nombre": "Lorraine",
-      "descripcion": "Wise",
-      "precio": "$124",
-      "tipo": "Comveyer",
-      "descuento": false
-    }, {
-      "nombre": "Nancy",
-      "descripcion": "Waters",
-      "precio": "$75",
-      "company": "Fuelton",
-      "employed": false
-    }];
-    $scope.data = {
-      model: null,
-      list: [{
-        "name": "nombre1",
-        "id": "1"
-      }, {
-        "name": "nombre2",
-        "id": "2"
-      }, {
-        "name": "nombre3",
-        "id": "3"
-      }, ]
-    };
+    .controller('productsCtrl', function ($scope, FileUploader) {
+        $scope.myData = [{
+            "nombre": "Cox",
+            "descripcion": "Carney",
+            "precio": "$10",
+            "tipo": "Enormo",
+            "descuento": true
+        }, {
+            "nombre": "Lorraine",
+            "descripcion": "Wise",
+            "precio": "$124",
+            "tipo": "Comveyer",
+            "descuento": false
+        }, {
+            "nombre": "Nancy",
+            "descripcion": "Waters",
+            "precio": "$75",
+            "company": "Fuelton",
+            "employed": false
+        }];
+        $scope.data = {
+            model: null,
+            list: [{
+                "name": "nombre1",
+                "id": "1"
+            }, {
+                "name": "nombre2",
+                "id": "2"
+            }, {
+                "name": "nombre3",
+                "id": "3"
+            }, ]
+        };
 
-    var uploader = $scope.uploader = new FileUploader({
-      url: 'http://localhost/personaSlim/ws1/images'
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'http://localhost/personaSlim/ws1/images'
+        });
+
+
+        /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*/
+
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function (item /*{File|FileLikeObject}*/ , options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+
+        $scope.change = function (value) {
+            console.info("element", value);
+        }
+
+        // CALLBACKS
+
+        /*        uploader.onWhenAddingFileFailed = function(item {File|FileLikeObject}, filter, options) {
+                    console.info('onWhenAddingFileFailed', item, filter, options);
+        		};
+                uploader.onAfterAddingFile = function(fileItem) {
+                    console.info('onAfterAddingFile', fileItem);
+                };
+                uploader.onAfterAddingAll = function(addedFileItems) {
+                    console.info('onAfterAddingAll', addedFileItems);
+                };
+                uploader.onBeforeUploadItem = function(item) {
+                    console.info('onBeforeUploadItem', item);
+                };
+                uploader.onProgressItem = function(fileItem, progress) {
+                    console.info('onProgressItem', fileItem, progress);
+                };
+                uploader.onProgressAll = function(progress) {
+                    console.info('onProgressAll', progress);
+                };
+                uploader.onSuccessItem = function(fileItem, response, status, headers) {
+                    console.info('onSuccessItem', fileItem, response, status, headers);
+                };
+                uploader.onErrorItem = function(fileItem, response, status, headers) {
+                    console.info('onErrorItem', fileItem, response, status, headers);
+                };
+                uploader.onCancelItem = function(fileItem, response, status, headers) {
+                    console.info('onCancelItem', fileItem, response, status, headers);
+                };
+                uploader.onCompleteItem = function(fileItem, response, status, headers) {
+                    console.info('onCompleteItem', fileItem, response, status, headers);
+                };
+                uploader.onCompleteAll = function() {
+                    console.info('onCompleteAll');
+                };*/
+
+
+        /* -----------------------     carousel       -------------------------  */
+
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+
+        $scope.addSlide = function () {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: '//unsplash.it/' + newWidth + '/300',
+                text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+                id: currIndex++
+            });
+        };
+
+
+        for (var i = 0; i < 3; i++) {
+            $scope.addSlide();
+        }
+
+
     });
-
-
-    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*/
-
-    // FILTERS
-
-    uploader.filters.push({
-      name: 'imageFilter',
-      fn: function (item /*{File|FileLikeObject}*/ , options) {
-        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
-      }
-    });
-
-
-    $scope.change = function (value) {
-      console.info("element", value);
-    }
-
-    // CALLBACKS
-
-    /*        uploader.onWhenAddingFileFailed = function(item {File|FileLikeObject}, filter, options) {
-                console.info('onWhenAddingFileFailed', item, filter, options);
-    		};
-            uploader.onAfterAddingFile = function(fileItem) {
-                console.info('onAfterAddingFile', fileItem);
-            };
-            uploader.onAfterAddingAll = function(addedFileItems) {
-                console.info('onAfterAddingAll', addedFileItems);
-            };
-            uploader.onBeforeUploadItem = function(item) {
-                console.info('onBeforeUploadItem', item);
-            };
-            uploader.onProgressItem = function(fileItem, progress) {
-                console.info('onProgressItem', fileItem, progress);
-            };
-            uploader.onProgressAll = function(progress) {
-                console.info('onProgressAll', progress);
-            };
-            uploader.onSuccessItem = function(fileItem, response, status, headers) {
-                console.info('onSuccessItem', fileItem, response, status, headers);
-            };
-            uploader.onErrorItem = function(fileItem, response, status, headers) {
-                console.info('onErrorItem', fileItem, response, status, headers);
-            };
-            uploader.onCancelItem = function(fileItem, response, status, headers) {
-                console.info('onCancelItem', fileItem, response, status, headers);
-            };
-            uploader.onCompleteItem = function(fileItem, response, status, headers) {
-                console.info('onCompleteItem', fileItem, response, status, headers);
-            };
-            uploader.onCompleteAll = function() {
-                console.info('onCompleteAll');
-            };*/
-
-
-  });
