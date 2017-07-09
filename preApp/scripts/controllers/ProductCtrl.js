9c9a14058605883ac0bd47521b42d2ad58279eac
lucasbonanni <lucas_eb2004@yahoo.com.ar>
2017-07-09 16:37:29 -0300

    product page and controller clean up

--- preApp/scripts/controllers/ProductCtrl.js
+++ preApp/scripts/controllers/ProductCtrl.js
@@ -11,7 +11,6 @@
  angular.module('tplaboratorioIv2016App')
    .controller('ProductCtrl', function ($scope,$cookies,productos,$state){
      var carrito = [];
-     $scope.slides = [{id:1,image:'',text:''},{id:2,image:'',text:''},{id:3,image:'',text:''}];
      $scope.cartCantidad = 0;
      $scope.product = {};
      var productId = $state.params['id'];
@@ -19,28 +18,11 @@
 
      productos.BuscarPorId(productId).then(function(response){
        $scope.product = response.data;
-
-       console.info($scope.product);
-       $scope.slides.push({
-          image:response.data.image1,
-          text:'a',
-          id:4
-        });
-        $scope.slides.push({
-          image:response.data.image2,
-          text:'b',
-          id:5
-        });
-        $scope.slides.push({
-          image:response.data.image3,
-          text:'c',
-          id:6
-        });
-        // $scope.$apply();
        $('a[rel*=lightbox]').lightBox();
      });
 
-      var storeValues = $cookies.getObject('carrito');
+     var storeValues = $cookies.getObject('carrito');
+
      if(storeValues != null){
        carrito = storeValues;
      }
@@ -54,45 +36,4 @@
        $cookies.putObject('carrito', carrito, {'expires': expireDate});
      };
 
-
-    //  $scope.slides.push({
-    //       image:'http://localhost/personaslim/uploads/2727-large.jpg' ,
-    //       text:'asdfasf',
-    //       id:0
-    //     });
-    //     $scope.slides.push({
-    //       image:'http://localhost/personaslim/uploads/2728-large.jpg' ,
-    //       text:'asdfasdf',
-    //       id:1
-    //     });
-    //     $scope.slides.push({
-    //       image:'http://localhost/personaslim/uploads/2729-large.jpg' ,
-    //       text:'asdfsaf',
-    //       id:2
-    //     });
-
-    
-
-    //  $scope.product ={
-    //         "id":1,
-    //         "nombre": "Cox",
-    //         "descripcion": "Carney",
-    //         "banda":"banda",
-    //         "cantidad":50,
-    //         "precio": "10",
-    //         "tipo": "Enormo",
-    //         "descuento": true,
-    //         "data": $scope.slides
-    //     };
-        
-        /***** carousel  */
-
-
-         $scope.myInterval = 3000;
-          $scope.noWrapSlides = false;
-          $scope.active = 0;
-          var currIndex = 0;
-
-
-          console.info($scope.slides);
    });
