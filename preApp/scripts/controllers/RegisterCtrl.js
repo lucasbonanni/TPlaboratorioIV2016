a4f3e26525a8aa406630af6c437794c46a78bf95
lucasbonanni <lucas_eb2004@yahoo.com.ar>
2017-07-24 14:34:40 -0300

    Add methods to register an login in register screen

--- preApp/scripts/controllers/RegisterCtrl.js
+++ preApp/scripts/controllers/RegisterCtrl.js
@@ -9,12 +9,39 @@
  */
 
 angular.module('tplaboratorioIv2016App')
-  .controller('RegisterCtrl', function($scope, $stateParams)  {
-    $scope.email = '';
-    console.info($stateParams);
+  .controller('RegisterCtrl', function($scope, $stateParams,$state, servicioLogin, users)  {
+    var loguarse = loguarse;
+    var registrarse = registrarse;
+    
+    $scope.usuario = {};
+    // $scope.usuario.email = '';
+    // $scope.usuario.nombre = '';
+    // $scope.usuario.password = '';
     if ($stateParams.mail !== undefined && $stateParams.mail !== '') {
-      $scope.email = $stateParams.mail;
+      $scope.usuario.email = $stateParams.mail;
       console.info($stateParams.mail);
+
+      
     }
+
+
+    function registrarse(){
+      users.Agregar($scope.usuario);
+      loguarse();
+    }
+
+
+    function loguarse(){
+        servicioLogin.doLogin($scope.usuario).then (function(rta){
+          $state.go('shop.home');
+        }, function(error){
+          servicioLogin.doLogin($scope.usuario).then (function(rta){
+            $state.go('shop.home');
+          }, function(error){
+            alert(error);
+          });
+        });
+      }
+
 
   });
