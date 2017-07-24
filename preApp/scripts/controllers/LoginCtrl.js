f4f417a78510eeb09d65e01500b1ba2857215205
lucasbonanni <lucas_eb2004@yahoo.com.ar>
2017-07-24 12:05:55 -0300

    Add users buttons to login screens to quick  test

--- preApp/scripts/controllers/LoginCtrl.js
+++ preApp/scripts/controllers/LoginCtrl.js
@@ -8,13 +8,65 @@
  * Controller of the tplaboratorioIv2016App
  */
 
- angular.module('tplaboratorioIv2016App')
-   .controller('LoginCtrl', function ($scope, $state){
-     $scope.email = '';
+angular.module('tplaboratorioIv2016App')
+    .controller('LoginCtrl', function ($scope, $state, servicioLogin) {
+        $scope.email = '';
+        $scope.usuario = {};
+        $scope.loguarse = loguarse;
+        this.goHome = goHome;
+        $scope.cliente = cliente;
+        $scope.empleado = empleado;
+        $scope.encargado = encargado;
+        $scope.administrador = administrador;
+        $scope.registrar = function () {
+            if ($scope.email !== '') {
+                $state.go('shop.register', {
+                    mail: $scope.email
+                });
+            }
+        };
 
-     $scope.registrar = function(){
-       if($scope.email !== ''){
-          $state.go('shop.register',{mail:$scope.email});
-       }
-     };
-   });
+
+        function loguarse() {
+            servicioLogin.doLogin($scope.usuario).then(function (rta) {
+                goHome();
+            }, function (error) {
+                servicioLogin.doLogin($scope.usuario).then(function (rta) {
+                    goHome();
+                }, function (error) {
+                    alert(error);
+                });
+            });
+        }
+
+        function goHome() {
+          $state.go('shop.home');
+        }
+        
+        function cliente() {
+            $scope.usuario.email = 'amerigot1@bravesites.com';
+            $scope.usuario.password = '53507';
+        }
+
+        function empleado() {
+            $scope.usuario.email = 'amerigot1@bravesites.com';
+            $scope.usuario.password = '53507';
+        }
+
+        function encargado() {
+            $scope.usuario.email = 'amerigot1@bravesites.com';
+            $scope.usuario.password = '53507';
+        }
+
+        function administrador() {
+            $scope.usuario.email = 'amerigot1@bravesites.com';
+            $scope.usuario.password = '53507';
+        }
+
+
+
+        // if ($stateParams.error !== undefined && !$stateParams.error !== '') {
+        //   $scope.email = $stateParams.mail;
+        //   console.info($stateParams.mail);
+        // }
+    });
